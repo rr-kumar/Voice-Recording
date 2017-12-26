ac91611c0e69a4de7d60d57343d970099364cc29
Rajeev <rr_kumar@aol.com>
2017-12-27 00:02:05 +0530

    Audio recorder file: Audio Session, File type and path to save has been successfully integrated in app.

---
+++ Voice Recording/RecordingViewController.swift
@@ -0,0 +1,71 @@
+//
+//  RecordingViewController.swift
+//  Voice Recording
+//
+//  Created by Apple on 16/12/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordingViewController: UIViewController {
+    
+    @IBOutlet weak var textfield: UITextField!
+    @IBOutlet weak var recordobutton: UIButton!
+    
+    var audioRecorder : AVAudioRecorder?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupRecorder()
+    }
+    
+    func setupRecorder(){
+        do {
+            // Create an audio session
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+            try session.overrideOutputAudioPort(.speaker)
+            try session.setActive(true)
+            
+            
+            // create URL for audio file
+            
+            let basepath: String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
+            let pathComponents = [basepath,"audio.mp3"]
+            let audioURL = NSURL.fileURL(withPathComponents: pathComponents)
+            
+            // audio recorder settings
+            
+            var settings : [String:AnyObject] = [:]
+            settings[AVFormatIDKey] = kAudioFileMP3Type as AnyObject
+            settings[AVSampleRateKey] = 44100.0 as AnyObject
+            settings[AVNumberOfChannelsKey] = 2 as AnyObject
+            
+            // audio recoder file saving loaction
+            audioRecorder = try AVAudioRecorder(url: audioURL!, settings:settings )
+            audioRecorder?.prepareToRecord()
+            
+        }catch _ as NSError{
+            
+        }
+    }
+    @IBAction func recordbutton(_ sender: Any) {
+    }
+    
+    @IBAction func playbutton(_ sender: Any) {
+    }
+    
+    @IBAction func savebutton(_ sender: Any) {
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+}
